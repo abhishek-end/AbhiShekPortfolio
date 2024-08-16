6cc9fb5596e315cdc841c147efdcb898a6b1df2a
abhishek-end <abhishekyadav49534@gmail.com>
2024-08-16 19:13:20 +0530

    try 5 font

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -44,7 +44,7 @@ const GetInTouch = () => {
   }, []);
 
   return (
-    <div className='main h-screen w-full relative overflow-hidden rounded-sm lg:flex lg:justify-center  '>
+    <div className='main h-screen w-full relative overflow-hidden rounded-sm lg:flex lg:justify-center '>
       {/* top-div */}
       <div
         id='top'
